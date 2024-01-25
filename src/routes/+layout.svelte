d338f95e97e63f6f661c11af05535b52693d87ff
David Smit <davidsmit@Davids-MacBook-Air.local>
2024-01-24 19:47:25 -0500

    Finished landing page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+</svelte:head>
+<script>
+    import "$lib/css/main.css"
+</script>
+<style>
+    body {
+        background-image: var(--bg-image);
+        background-position: center;
+        background-size: cover;
+    }
+    body {
+        width: 100vw;
+        margin-left: auto;
+        margin-right: auto;
+        
+    }
+</style>
+<body>
+    <slot></slot>
+</body>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    .hero {
+        display: flex;
+        align-items: center;
+        text-align: center;
+        flex-direction: row;
+        height: 100vh;
+    }
+    .banner {
+        backdrop-filter: blur(40px);
+
+    }
+    .hero h1 {
+        backdrop-filter: blur(50px);
+    }
+    .hero image {
+        width: 20px;
+    }
+
+    
+</style>
+<script>
+    import logo from '$lib/images/Logo.png'
+</script>
+
+<div class="hero">
+    <div class="banner">
+    <img src="{logo}" alt="Logo"/>    
+    <h1>
+        We're working to build you a great page. In the meantime, feel free to call
+        at <a href="tel:7048809955">(704) 880-9955</a>
+    </h1>
+</div>
+</div>
